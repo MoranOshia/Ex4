089b47d09bc2db9dc7d999a4d876016a3858061c
MoranOshia <moranio104@gmail.com>
2019-01-12 18:01:58 +0200

    thread user

--- src/Thread/ThreadPlay.java
+++ src/Thread/ThreadPlay.java
@@ -7,19 +7,30 @@ import Robot.Fruit;
 import Robot.Game;
 import Robot.Packman;
 import Robot.Play;
-
+/*
+ * This class is responsible for the Thread of the users play.
+ */
 public class ThreadPlay extends Thread{
 	private Play play1 ;
 	private Game game ;
 	private Game gameCopy ;
 	private window w;
-
+	/*
+	 * Copy constructor for values
+	 *  @param pl - current play object
+	 *  @param gam- current game object
+	 *  @param copy-copy of the game
+	 *  @param window- the current window of the game
+	 */
 	public ThreadPlay(Play pl, Game gam, Game copy, window w) {
 		this.play1 = pl;
 		this.game = gam;
 		this.gameCopy = copy;
 		this.w = w;
 	}
+	/*
+	 *This function is responsible for running the game automatically with the Threads, and updating the locations of the game
+	 */
 	public void run() {
 		play1.start();
 		while(play1.isRuning()) {
